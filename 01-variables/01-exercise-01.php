d3364fb71d47adb5880715a8866b005f40ead10a
Vladislav P <113527973+LvVlad@users.noreply.github.com>
2023-03-21 16:12:29 +0200

    Add files via upload

---
+++ 01-variables/01-exercise-01.php
@@ -0,0 +1,10 @@
+<?php
+
+//Create variable that prints out an integer 10, float 10.10, string "hello world"
+
+$number = 10;
+$float = 10.10;
+$string = 'hello world';
+
+$print = print "$number, $float, $string";
+//echo '10, 10.10, hello world'
