337b6687f325a4ca22569ed94326258ff8d367a0
Vladislav P <113527973+LvVlad@users.noreply.github.com>
2023-03-21 16:04:24 +0200

    Add files via upload

---
+++ 01-exercise-02.php
@@ -0,0 +1,8 @@
+<?php
+
+//Dump the same values that should display both data type & its value. (Note, usage of var_dump)
+
+$valueOne = 10;
+$valueTwo = '10';
+
+var_dump($valueOne, $valueTwo);
---
+++ 01-exercise-03.php
@@ -0,0 +1,11 @@
+<?php
+
+//Concatenate your name, surname and integer of your age.
+
+$name = 'Vladislavs';
+$surname = 'Panfilovs';
+$age = 27;
+
+//$full = $name.' '.$surname.' '.$age;
+$full = "$name $surname $age";
+echo $full;
